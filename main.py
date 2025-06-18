44381c1029858ed1a8e63b19b21a71bc26c0fd2b
flumperdump <flumperdump@hotmail.com>
2025-06-18 00:33:37 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from quicktrade import launch_app
 
 if __name__ == '__main__':
-    launch_app()
+    import sys
+    from PyQt6.QtWidgets import QApplication
+    app = QApplication(sys.argv)
+
+    from dashboard import MainWindow
+    window = MainWindow()
+    window.show()
+
+    sys.exit(app.exec())
+
