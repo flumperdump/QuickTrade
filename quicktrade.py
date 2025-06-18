ca7ce867cae67b0c982c2634f5aceee2249276f8
flumperdump <flumperdump@hotmail.com>
2025-06-18 00:34:35 -0700

    Update quicktrade.py

--- quicktrade.py
+++ quicktrade.py
@@ -1,20 +1,13 @@
 from PyQt6.QtWidgets import QApplication
 import sys
+
 from dashboard import MainWindow
 
-def launch_app():
+def main():
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
 
 if __name__ == '__main__':
-    import sys
-    from PyQt6.QtWidgets import QApplication
-    app = QApplication(sys.argv)
-
-    from dashboard import MainWindow
-    window = MainWindow()
-    window.show()
-
-    sys.exit(app.exec())
+    main()
