8165fef73827c21d80acde46030ecd470a3eabe7
flumperdump <flumperdump@hotmail.com>
2025-06-18 20:01:04 -0700

    Delete QuickTrade_v1.0_FullPackage directory

--- QuickTrade_v1.0_FullPackage/main.py
+++
@@ -1,4 +0,0 @@
-from quicktrade import launch_app
-
-if __name__ == '__main__':
-    launch_app()
--- QuickTrade_v1.0_FullPackage/quicktrade.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtWidgets import QApplication
-import sys
-from dashboard import MainWindow
-
-def launch_app():
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
